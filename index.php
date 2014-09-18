80a935b25b8d97e3b10a2e5c66e26c47f47b5dff
alveiromartinez <alveiromartinezbotello@gmail.com>
2014-09-18 13:47:56 +1000

    deploy version4

--- index.php
+++ index.php
@@ -1,54 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>ICT Careers</title>
-		<link href="css/css.css" rel="stylesheet" type="text/css" />
-		<link href="css/Stylesheet.css" rel="stylesheet" type="text/css">
-		<link href='http://fonts.googleapis.com/css?family=Signika+Negative' rel='stylesheet' type='text/css' />
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ICT Careers</title>
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/local.css" />
 
-  </head>
+    <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- you need to include the shieldui css and js assets in order for the charts to work -->
+    <link rel="stylesheet" type="text/css" href="http://www.shieldui.com/shared/components/latest/css/shieldui-all.min.css" />
+    <link rel="stylesheet" type="text/css" href="http://www.shieldui.com/shared/components/latest/css/light-bootstrap/all.min.css" />
+    <script type="text/javascript" src="http://www.shieldui.com/shared/components/latest/js/shieldui-all.min.js"></script>
+    <script type="text/javascript" src="http://www.prepbootstrap.com/Content/js/gridData.js"></script>
+</head>
+
   <body>
-<!--div id="logo"></div-->
-<div id="header">
-		<ul>
-			<li><a href="main.htm" class="whitelink">Home</a></li>
-      <li><a href="#" class="whitelink">About</a></li>
-      <li><a href="#" class="whitelink">Contact us</a></li>
-    </ul>
-</div>
-<div id="banner">
-	<div id="bannercenter">
-  		<div id="speech" class="class_box_shadow">ICT Careers<div class="sh_bottom_left"></div>
-   		</div>
+   <div id="wrapper">
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">ICT Careers</a>
+            </div>
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li><a href="#">Student</a></li>
+                    <li><a href="teacher_main.htm">Skill&Improvement</a></li>
+                    <li><a href="parent_act.htm">Activities</a></li>
+                    <li><a href="parent_career.htm">Careers</a></li>
+                    <li><a href="parent_scholarship.htm">Scholarships</a></li>
+                    <li><a href="#">Advisors</a></li>					
+                </ul>
+                <ul class="nav navbar-nav navbar-right navbar-user">
+				    <li class="active"><a href="main.htm">Home</a></li>
+					<li><a href="#about">About</a></li>
+					<li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Home</h1>
+                    <div class="alert alert-success alert-dismissable">
+                        Welcome ICT central information.
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-	 <ul class="fourboxes">
-  		<li class="first"><a href="#"><p style="color:black">Students</p></a></li>
- 			<li><a href="teacher_main.htm"><p style="color:black">Teachers</p></a></li>
-  		<li><a href="parent_main.htm"><p style="color:black">Parents</p></a></li>
-			<li><a href="#"><p style="color:black">Advisors</p></a></li>
-	 </ul>
-            
-<div id="maincontent">
-<h1>Welcome to ICT Careers.<br/>
-  <br />
-</h1>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-</div> <p style="clear:both">&nbsp;</p>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
